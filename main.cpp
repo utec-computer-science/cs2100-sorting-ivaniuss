78c276986b00d516d1d50be23b748b3390b277a1
ivan mamani <46487626+ivaniuss@users.noreply.github.com>
2020-03-29 20:04:51 -0500

    Docs(main)Implementando los algoritmos sorted

--- main.cpp
+++ main.cpp
@@ -12,8 +12,15 @@ int main(int, char*[]){
     vector<int> v{5,6,1,8,3,6,8,5,2,7,9,3,9,12,8};
 
     sorting s1{v};
+    
+//    cout<<s1.InsertSort(v);
+//    cout<<s1.SelectionSort(v);
+//    cout<<s1.BubbleSort(v);
+//    cout<<s1.MergeSort(v);
+//    s1.QuickSort(v,0,v.size()-1);
+//    cout<<s1.ShellSort(v);
+//    cout<<s1.BinSort(v);
 
-    s1.QuickSort(v,0,v.size()-1);
     return 0;
 
 }
